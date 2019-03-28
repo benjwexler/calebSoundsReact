cab8c2c299321d4546e6aaf8a85e7ee3ebc3c2aa
Ben Wexler <mixedywex@gmail.com>
2019-03-28 19:48:09 -0400

    I like the accidental styling of show track so far

--- app/javascript/bundles/HelloWorld/components/EditTrack.jsx
+++ app/javascript/bundles/HelloWorld/components/EditTrack.jsx
@@ -110,27 +110,6 @@ class EditTrack extends React.Component {
     console.log(appleMusicURL);
     console.log(soundcloudURL);
 
-
-    var formData = new FormData();
-
-    // formData.append("username", "Groucho");
-    // formData.append("accountnum", 123456); // number 123456 is immediately converted to a string "123456"
-    
-    // // HTML file input, chosen by user
-    // formData.append("userfile", fileInputElement.files[0]);
-    
-    // // JavaScript file-like object
-    // var content = '<a id="a"><b id="b">hey!</b></a>'; // the body of the new file...
-    // var blob = new Blob([content], { type: "text/xml"});
-    
-    // formData.append("webmasterfile", blob);
-    
-    // var request = new XMLHttpRequest();
-    // request.open("POST", "http://foo.com/submitform.php");
-    // request.send(formData);
-
-
-
   }
 
   componentDidMount() {
