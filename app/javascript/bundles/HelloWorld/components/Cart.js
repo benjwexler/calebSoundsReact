aeea690dd9a35b1749c6044a56abfc1466a8484a
Ben Wexler <mixedywex@gmail.com>
2019-04-14 15:53:59 -0400

    styling empty shopping cart

--- app/javascript/bundles/HelloWorld/components/Cart.js
+++ app/javascript/bundles/HelloWorld/components/Cart.js
@@ -1,34 +1,53 @@
-import React from 'react';
+import React from "react";
 
+const cart = props => {
+  console.log(props.isCartEmpty);
+  let cartContent;
 
-const cart = (props) => {
-
-    console.log(props.isCartEmpty)
-    let proceedToCheckout;
-
-    if (!props.isCartEmpty) {
-        proceedToCheckout = (
-            <a href="/checkout" id="checkoutText">Proceed to Checkout: {props.totalPrice}</a>
-        )
-    }
-
-    return (
-        <div id="cart" className={props.showCartBoolean + " " + props.cartHeightZero} onTransitionEnd={props.cartTransitionEnd}>
-            <div onClick={props.toggleCart} className="exitCart" id="exitModal"><i id="exitCartIcon" className="far fa-times-circle"></i></div>
-            <div id="yourCartText">Your Cart</div>
-            {props.items}
-            <div id="cartTotal">
-                <div id="totalLeft">Total:</div>
-                <div id="totalRight">{props.totalPrice}</div>
-            </div>
-            <div onClick={props.clearCart} id="emptyBagContainer"> <span id="emptyBagText">Empty Bag</span></div>
-            <div id="checkoutBtnOuterContainer">
-            <div id="checkoutBtn">
-                {proceedToCheckout}
-            </div>
+  if (!props.isCartEmpty) {
+    cartContent = (
+        <React.Fragment>
+        <div id="yourCartText">Your Cart</div>
+        {props.items}
+        <div id="cartTotal">
+          <div id="totalLeft">Total:</div>
+          <div id="totalRight">{props.totalPrice}</div>
         </div>
+        <div onClick={props.clearCart} id="emptyBagContainer">
+          {" "}
+          <span id="emptyBagText">Empty Bag</span>
         </div>
+        <div id="checkoutBtnOuterContainer">
+          <div id="checkoutBtn">
+            <a href="/checkout" id="checkoutText">
+              Proceed to Checkout: {props.totalPrice}
+            </a>
+          </div>
+        </div>
+        </React.Fragment>
+    );
+  } else {
+    cartContent = (
+        <div className="cartEmpty"> 
+        Your Cart is Empty 
+        <br/>
+        <br/>
+        <span className="frown">:(</span> </div>
     )
+  }
+
+  return (
+    <div
+      id="cart"
+      className={props.showCartBoolean + " " + props.cartHeightZero}
+      onTransitionEnd={props.cartTransitionEnd}
+    >
+      <div onClick={props.toggleCart} className="exitCart" id="exitModal">
+        <i id="exitCartIcon" className="far fa-times-circle" />
+      </div>
+      {cartContent}
+    </div>
+  );
 };
 
 export default cart;
