c6b9b69aad620c8bcd6c2c2377564452418576ec
Ben Wexler <mixedywex@gmail.com>
2019-04-22 20:59:03 -0400

    can fully edit and delete tracks async

--- app/javascript/bundles/HelloWorld/components/EditTrackModal.js
+++ app/javascript/bundles/HelloWorld/components/EditTrackModal.js
@@ -169,78 +169,22 @@ const EditTrackModal = props => {
             </div>
           </div>
           <br />
+          <div className="modalIconContainer">
+     <DatePicker
+            selected={props.date}
+            onChange={props.handleDateChange}
+            peekNextMonth
+            showMonthDropdown
+            showYearDropdown
+            dropdownMode="select"
+            placeholderText="Release Date"
+            />
+            <i className="far fa-calendar modalIcon" style={{top: '43%'}}/>
+            {/* <i className="fas fa-calendar-week modalIcon" /> */}
+            </div>
 
-          <div class="field">
-            <label for="track_release_date">Release date</label>
-            <br />
-            <select onChange={props.onChange} id="track_release_date_1i" name="track[release_date(1i)]">
-              <option value="2014">2014</option>
-              <option value="2015">2015</option>
-              <option value="2016">2016</option>
-              <option value="2017">2017</option>
-              <option value="2018">2018</option>
-              <option value="2019" selected="selected">
-                2019
-              </option>
-              <option value="2020">2020</option>
-              <option value="2021">2021</option>
-              <option value="2022">2022</option>
-              <option value="2023">2023</option>
-              <option value="2024">2024</option>
-            </select>
-            <select onChange={props.onChange} id="track_release_date_2i" name="track[release_date(2i)]">
-              <option value="1">January</option>
-              <option value="2">February</option>
-              <option value="3" selected="selected">
-                March
-              </option>
-              <option value="4">April</option>
-              <option value="5">May</option>
-              <option value="6">June</option>
-              <option value="7">July</option>
-              <option value="8">August</option>
-              <option value="9">September</option>
-              <option value="10">October</option>
-              <option value="11">November</option>
-              <option value="12">December</option>
-            </select>
-            <select onChange={props.onChange} id="track_release_date_3i" name="track[release_date(3i)]">
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-              <option value="11">11</option>
-              <option value="12">12</option>
-              <option value="13">13</option>
-              <option value="14">14</option>
-              <option value="15">15</option>
-              <option value="16">16</option>
-              <option value="17">17</option>
-              <option value="18">18</option>
-              <option value="19">19</option>
-              <option value="20">20</option>
-              <option value="21">21</option>
-              <option value="22">22</option>
-              <option value="23">23</option>
-              <option value="24">24</option>
-              <option value="25">25</option>
-              <option value="26" selected="selected">
-                26
-              </option>
-              <option value="27">27</option>
-              <option value="28">28</option>
-              <option value="29">29</option>
-              <option value="30">30</option>
-              <option value="31">31</option>
-            </select>
-          </div>
 
+          <br />
           <br />
 
           <div
@@ -255,24 +199,11 @@ const EditTrackModal = props => {
               name="commit"
               value="Edit Track"
               data-disable-with="Create Track"
+
               // disabled='disabled'
             />
           </div>
-          <DatePicker
-  // selected={new Date()}
-  // onChange={props.handleDateChange}
-  // dateFormatCalendar={"MMM yyyy"}
-  // minDate={subMonths(new Date(), 6)}
-  // maxDate={addMonths(new Date(), 6)}
-  // showMonthYearDropdown
-
-  selected={props.date}
-  onChange={props.handleDateChange}
-    peekNextMonth
-    showMonthDropdown
-    showYearDropdown
-    dropdownMode="select"
-/>
+         
 
 
         </form>
